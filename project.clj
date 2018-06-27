729d26f88aabb9389f697d607986f557576312c9
Ted Cushman <biiwide@users.noreply.github.com>
2018-06-26 21:20:53 -0500

    Updating license and being more pessimistic about version.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject biiwide/applicative "0.1.0-SNAPSHOT"
+(defproject biiwide/applicative "0.0.1-SNAPSHOT"
 
   :description "Applicative programming and data transformations"
 
   :url "http://github.com/biiwide/applicative"
 
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Eclipse Public License 2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]]
 
@@ -18,5 +18,14 @@
 ;  :jar-exclusions [#"basic.*\.clj" #"^resources/public"]
 
   :gorilla-options {:load-scan-exclude #{".git" ".svn" "bin"}}
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
 
   )
